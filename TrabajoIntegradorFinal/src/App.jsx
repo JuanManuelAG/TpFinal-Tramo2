2d67bba7d8c9d6da8a678b6b206cdca11956bd0f
JLuuz22 <106003508+JLuuz22@users.noreply.github.com>
2023-11-24 23:48:21 -0300

    localstorage

--- TrabajoIntegradorFinal/src/App.jsx
+++ TrabajoIntegradorFinal/src/App.jsx
@@ -1,23 +1,35 @@
 import TaskList from './componentes/TaskList/TaskList.jsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import TaskForm from './componentes/TaskForm/TaskForm';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 
 function App() {
 
   const [taskList, setTaskList] = useState([]);
 
+
+  useEffect(() => {
+    const storedList = localStorage.getItem('lista');
+    if (storedList) {
+      setTaskList(JSON.parse(storedList));
+    }
+  }, []);
+
   
  
   
   const handleAddTask = (newTask) => {
-    setTaskList([...taskList, newTask]);
-    setShowTaskForm(false);
+    const updatedList = [...taskList, newTask];
+    setTaskList(updatedList);
+    localStorage.setItem('lista', JSON.stringify(updatedList));
   };
+
+  
   const handleDelete = (taskId) => {
     const filteredTasks = taskList.filter((task) => task.id !== taskId);
     setTaskList(filteredTasks);
+    localStorage.setItem('lista', JSON.stringify(filteredTasks));
   };
 
   return (
